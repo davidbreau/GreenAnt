5568b6d9bad31f6e839afa092ef73e71790cf4aa
Olivier <olivierkotwica@gmail.com>
2023-03-30 15:40:42 +0200

    ajout def update pwd et mail

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ async def root():
 
 @app.post("/api/auth/inscription")
 async def inscription(user:UserRegister):
-    if len(crud.get_users_by_mail(user.mail)) > 0:
+    if crud.get_user_id_from_mail(user.mail) is not None:
         raise HTTPException(status_code=403, detail="L'email fourni possède déjà un compte")
     else:
         id_user = crud.create_user(user.username, user.firstname, user.lastname, user.mail, hasher_mdp(user.password), None)
@@ -52,13 +52,14 @@ async def inscription(user:UserRegister):
             "email" : user.mail,
             "mdp" : user.password,
             "id" : id_user
+        # ici ajouter la date de creation du token pour augmenter la secu
         }, SECRET_KEY, algorithm=ALGORITHM)
         crud.update_token(id_user, token)
         return {"token" : token}
 
 @app.post("/api/auth/token")
 async def login_token(user:UserLogin):
-    resultat = crud.obtenir_jwt_depuis_email_mdp(user.mail, hasher_mdp(user.password))
+    resultat = crud.get_jwt_from_mail_password(user.mail, hasher_mdp(user.password))
     if resultat is None:
         raise HTTPException(status_code=401, detail="Login ou mot de passe invalide")
     else:
@@ -68,6 +69,7 @@ async def login_token(user:UserLogin):
 async def mes_articles(req: Request):
     try:
         decode = decoder_token(req.headers["Authorization"])
-        return {"id_user" : crud.get_id_user_by_email(decode["email"])[0]}
+        # ici verif si le token exist ds la base de donnee
+        return {"id_user" : crud.get_user_id_from_mail(decode["email"])[0]}
     except:
         raise HTTPException(status_code=401, detail="Vous devez être identifiés pour accéder à cet endpoint")
