38a0b69a1db49b61cfa3ac6a16e546a1ff39c9f6
davidbreau <davidbreau@live.fr>
2023-03-31 12:39:03 +0200

    docstrings crud.py + divers commentaires de codes

---
+++ create_actions.py
@@ -0,0 +1,14 @@
+import sqlite3
+from crud import create_action
+
+create_action('ALSTOM', 24.920)
+create_action('LVMH', 831.70)
+create_action('AIR FRANCE', 1.669)
+create_action("L'ORÉAL", 408.100)
+create_action('SOCIETE GENERALE', 20.565)
+create_action('EDENRED', 54.900)
+create_action('BIC', 58.580)
+create_action('ORANGE', 10.964)
+create_action('NOKIA', 4.462)
+create_action('RENAULT', 37.320)
+create_action('UBISOFT ENTERTAIN.', 24.140)
--- crud.py
+++ crud.py
@@ -1,18 +1,29 @@
 import sqlite3
 import datetime
-
+from typing import Union
 
 ################# USER AUTH :
 
-def get_jwt_from_mail_password(mail:str, password:str):
+
+def get_jwt_from_mail_password(mail:str, password:str)-> Union[str, None]:
+    """
+    Gets a JSON Web Token (JWT) from the database using the email and password.
+
+    Args:
+        mail (str): The email of the user.
+        password (str): The password of the user.
+
+    Returns:
+        The JWT if found, or None otherwise.
+    """    
     connexion = sqlite3.connect("bdd.db")
     curseur = connexion.cursor()
     curseur.execute("""
                     SELECT jwt FROM user WHERE mail=? AND password=?
                     """, (mail, password))
-    resultat = curseur.fetchone()
+    result = curseur.fetchone()
     connexion.close()
-    return resultat
+    return result
 
 # print(get_jwt_from_mail_password('dvdbr@googlemail.com', 'azerty2'))
 
@@ -20,7 +31,21 @@ def get_jwt_from_mail_password(mail:str, password:str):
 ########################################### CREATE #####################################################
 ########################################################################################################
 
-def create_user(username: str, first_name:str, last_name:str, mail:str, password:str, jwt:str):
+def create_user(username: str, first_name:str, last_name:str, mail:str, password:str, jwt:str) -> int:
+    """
+    Creates a new user in the database with the given informations.
+
+    Args:
+        username (str): The username of the user.
+        first_name (str): The first name of the user.
+        last_name (str): The last name of the user.
+        mail (str): The email of the user.
+        password (str): The password of the user.
+        jwt (str): The JSON Web Token (JWT) of the user.
+
+    Returns:
+        The ID of the new user as an integer.
+    """
     connexion = sqlite3.connect('bdd.db')
     curseur = connexion.cursor()
     curseur.execute("""
@@ -36,7 +61,14 @@ def create_user(username: str, first_name:str, last_name:str, mail:str, password
 # create_user('av','arnaud', 'vila', 'arnaudvila@gmail.com', '5678', '12345')
 # create_user('olive','olivier', 'kotwica', 'okot@live.fr', '900.', '123456')
 
-def create_action(company:str, value:float):
+def create_action(company:str, value:float) -> None:
+    """
+    Creates a new action in the database with the given informations.
+
+    Args:
+        company(str): The name of the action based on the Company name as shown in actions courts.
+        value(float): The initial value of the action (that will be set to updated values later on).
+    """
     connexion = sqlite3.connect('bdd.db')
     curseur = connexion.cursor()
     curseur.execute("""
@@ -46,17 +78,25 @@ def create_action(company:str, value:float):
     connexion.commit()
     connexion.close()
     
-def link_user_action(user_id:int, action_id:int):
+def link_user_action(user_id:int, action_id:int) -> None:
+    """
+    Creates a new entry in the user_action association table to simulate the act to buy an action.
+
+    Args:
+        user_id (int): The ID of the user who's buying the action.
+        action_id(int): The ID of the bought action.
+    """
     connexion = sqlite3.connect('bdd.db')
     curseur = connexion.cursor()
 
-    # Récupérer la valeur de l'action
+    # Get action's actual value from its ID
     curseur.execute("""
                  SELECT value FROM action WHERE id = ?
                  """, (action_id,))
     value = curseur.fetchone()[0]
 
-    # Enregistrer l'achat de l'utilisateur pour cette action
+    # Save the bought action in the associatable with the user id, the action id, and the presearched action's value.
+    # The null values are kept for the time the action will be sold
     curseur.execute("""
                  INSERT INTO user_action
                  VALUES (?, ?, ?, datetime('now'), NULL, NULL, NULL)
@@ -64,11 +104,19 @@ def link_user_action(user_id:int, action_id:int):
 
     connexion.commit()
     connexion.close()
-# link_user_action(2,1)
+    
+# link_user_action(1,1)
 # link_user_action(1,2)
 # link_user_action(3,3)
 
-def link_user_user(user_id_following:int, user_id_followed:int):
+def link_user_user(user_id_following:int, user_id_followed:int) -> None:
+    """
+    Creates a new entry in the user_user association table to simulate the action for one user to follow another user aiming to see his owned actions.
+
+    Args:
+        user_id_following(int): the id of the user who's following one other user.
+        user_id_followed(int): the id of the user who's now followed by the previously specified user.
+    """
     if user_id_following == user_id_followed:
         print("Erreur: un utilisateur ne peut pas se suivre lui-même.")
         return
@@ -91,7 +139,16 @@ def link_user_user(user_id_following:int, user_id_followed:int):
 ########################################### READ #######################################################
 ########################################################################################################
 
-def get_user_from_mail(mail:str):
+def get_user_from_mail(mail:str) -> list :
+    """
+    Get all the informations of an user if it correlates to the specified email.
+    
+    Args:
+        mail (str): The email of the user you want the complete informations of.
+
+    Returns:
+        A list containing a tuple with all the informations.
+    """
     connexion = sqlite3.connect("bdd.db")
     curseur = connexion.cursor()
     curseur.execute("""
@@ -103,7 +160,15 @@ def get_user_from_mail(mail:str):
 
 # print(get_user_from_mail('okot@live.fr'))
 
-def get_user_id_from_mail(mail:str):
+def get_user_id_from_mail(mail:str)-> tuple:
+    """"
+    Gets the id of the user if it correlated to the specified email
+    
+    Args:
+        mail(str): The email of the user you want the id of.
+    Returns :
+        A tuple containing the ID of the corresponding user
+    """
     connexion = sqlite3.connect("bdd.db")
     curseur = connexion.cursor()
     curseur.execute("""
@@ -113,9 +178,14 @@ def get_user_id_from_mail(mail:str):
     connexion.close()
     return result
 
-# print(get_user_id_from_mail('okot@live.fr'))
+# print(get_user_id_from_mail('davidbreau@live.fr'))
 
 def get_actions_list():
+    """
+        Retrieves all actions from the database.
+    Returns:
+        A list of tuples representing each action with the company name and the actual value.
+    """
     connexion = sqlite3.connect('bdd.db')
     curseur = connexion.cursor()
     curseur.execute("""
@@ -128,6 +198,11 @@ def get_actions_list():
 # print(get_actions_list())
 
 def get_users_list():
+    """
+    Retrieves all users from the database.
+    Returns:
+        A list of tuples representing each user.
+    """
     connexion = sqlite3.connect('bdd.db')
     curseur = connexion.cursor()
     curseur.execute("""
@@ -154,13 +229,23 @@ def get_user_id_from_jwt(jwt:str):
 
 # print(get_user_id_from_jwt('1234'))
 
-def get_user_s_actions_list(user_id:int):
+def get_user_s_actions_list(user_id:int)->list:
+    """
+    Shows all the action that are currently owned by the user detaining the specified ID, ignoring the actions that have been sold.
+    
+    Args:
+        user_id(int): The id of the user you want to see the current actions
+        
+    Returns:
+        A list of the actions bought but not yet sold.
+    """
     connexion = sqlite3.connect('bdd.db')
     curseur = connexion.cursor()
     curseur.execute("""
                     SELECT company, value FROM action
                         INNER JOIN user_action ON user_action.action_id = action.id
                         WHERE user_action.user_id = ? 
+                            and user_action.sold != True
                     """, (user_id,))
     result = curseur.fetchall()
     connexion.close()
@@ -168,22 +253,40 @@ def get_user_s_actions_list(user_id:int):
 
 # print(get_user_s_actions_list(2))
 
-def get_user_s_actions_sum(user_id:int):
+def get_user_s_actions_sum(user_id:int)-> Union[int, None]:
+    """
+    Calculates the sum of the actions currently owned by the specified user to get the capital
+    
+    Args:
+        user_id(int): The ID of the user
+    Returns:
+        The capital of the user as an integer, or None if the user don't have any action.
+    """
     connexion = sqlite3.connect('bdd.db')
     curseur = connexion.cursor()
     curseur.execute("""
                     SELECT SUM(action.value) AS capital
                         FROM action
                         INNER JOIN user_action ON user_action.action_id = action.id
-                        WHERE user_action.user_id = ? 
+                        WHERE user_action.user_id = ?
+                            and user_action.sold != True 
                     """, (user_id,))
     result = curseur.fetchall()
     connexion.close()
     return result[0]
 
-# print(get_user_s_actions_sum(1))
+print(get_user_s_actions_sum(1))
 
-def get_following_actions(user_id:int):
+def get_following_actions(user_id:int) -> list:
+    """
+    Shows individually all the actions that are owned by at least one followed user.
+    
+    Args:
+        user_id(int): The id of the user you want to get the list of actions from followed users.
+        
+    Returns:
+        A list of actions
+    """
     connexion = sqlite3.connect('bdd.db')
     curseur = connexion.cursor()
 
@@ -206,7 +309,16 @@ def get_following_actions(user_id:int):
     return actions
 # print(get_following_actions(3))
 
-def get_action_id_from_company(company:str):
+def get_action_id_from_company(company:str) -> int:
+    """
+    Returns the id of the actions that have the specified company name
+    
+    Args:
+        company(str): Name of the company
+        
+    Returns:
+        The ID as an integer
+    """
     connexion = sqlite3.connect('bdd.db')
     curseur = connexion.cursor()
     curseur.execute("""
@@ -217,6 +329,26 @@ def get_action_id_from_company(company:str):
     connexion.close()
     return result[0]
 
+def get_all_ids_followed(user_id:int) -> list:
+    """
+    Shows all the ids of the followed users from the specified user id.
+    
+    Args:
+        user_id(int): id of the user
+        
+    Returns:
+        A list containing a tuple of the ids
+    """
+    connexion = sqlite3.connect('bdd.db')
+    curseur = connexion.cursor()
+    curseur.execute("""
+                    SELECT user_id_followed from user_user
+                        WHERE user_id_following = ?
+                    """, (user_id,))
+    result = curseur.fetchall()
+    connexion.close()
+    return result
+
 # print(get_action_id_from_company('EDENRED'))
 
 ########################################################################################################
@@ -225,7 +357,14 @@ def get_action_id_from_company(company:str):
 
 
 
-def update_action_value(new_value:int, action_id:int):
+def update_action_value(new_value:float, action_id:int) -> None:
+    """
+    Update the value of a specified action from its id, and stores the old value and the time it changed in the action_value_change table.
+    
+    Args:
+        new_value(float): The value that will replaces its previous in the action table
+        action_id(int): The id of the action
+    """
     connexion = sqlite3.connect('bdd.db')
     curseur = connexion.cursor()
     curseur.execute("""
@@ -249,6 +388,11 @@ def update_action_value(new_value:int, action_id:int):
 # update_action_value(76,1)
     
 def update_user_action_sold(user_id:int, action_id:int):
+    """
+    Simulates the act from a user to sell it.
+    Will look for the actual value of the specified action and store it as sold_value, automatically adding sold_time,
+    And specifies that the action is now sold in the user_action table
+    """
     connexion = sqlite3.connect('bdd.db')
     curseur = connexion.cursor()
     curseur.execute("""
@@ -269,6 +413,9 @@ def update_user_action_sold(user_id:int, action_id:int):
 # update_user_action_sold(1,1)
     
 def update_token(id, token:str):
+    """
+    Changes the token of the specified user
+    """
     connexion = sqlite3.connect("bdd.db")
     curseur = connexion.cursor()
     curseur.execute("""
@@ -282,6 +429,9 @@ def update_token(id, token:str):
 # update_token(1, 7878)
     
 def update_mail(new_mail:str, user_id:int):
+    """
+    Updates the mail of the specified user.
+    """
     connexion = sqlite3.connect('bdd.db')
     curseur = connexion.cursor()
     curseur.execute("""
@@ -295,6 +445,9 @@ def update_mail(new_mail:str, user_id:int):
 # update_mail('davidbr@live.fr', 1)
 
 def update_password(new_password:str, user_id:int):
+    """
+    Updates the password of the specified user
+    """
     connexion = sqlite3.connect('bdd.db')
     curseur = connexion.cursor()
     curseur.execute("""
@@ -312,6 +465,9 @@ def update_password(new_password:str, user_id:int):
 ########################################################################################################
 
 def delete_user(user_id:int):
+    """
+    Deletes specified user, will also delete all entries from the database where the user's id is used.
+    """
     connexion = sqlite3.connect('bdd.db')
     curseur = connexion.cursor()
     curseur.execute("""
@@ -325,6 +481,9 @@ def delete_user(user_id:int):
 # delete_user(5)
 
 def delete_action(action_id:int):
+    """
+    Deletes specified action, will also delete all entries from the database where the action's id is used.
+    """
     connexion = sqlite3.connect('bdd.db')
     curseur = connexion.cursor()
     curseur.execute("""
@@ -337,6 +496,10 @@ def delete_action(action_id:int):
 # delete_action(3)
 
 def unlink_user_user(user_id_following:int, user_id_followed:int):
+    """
+    Simulates the the "unfollow" action.
+    Deletes the link of the two users in the user_user action
+    """
     connexion = sqlite3.connect('bdd.db')
     curseur = connexion.cursor()
     curseur.execute("""
